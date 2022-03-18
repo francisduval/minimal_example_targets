0fa2265bde6f4e4d012782cf988c2a86955c22d6
Francis Duval <francisduval37@gmail.com>
2022-03-18 12:49:30 -0400

    Target predictions of lm

--- _targets.R
+++ _targets.R
@@ -6,5 +6,7 @@ walk(fs::dir_ls("R"), source)
 list(
   tar_target(boston_file, "data/boston.csv", format = "file"),
   tar_target(boston_data, read_csv(boston_file)),
-  tar_target(plot_medv_lstat, make_scatter_plot(boston_data, x = lstat, y = medv))
+  tar_target(plot_medv_lstat, make_scatter_plot(boston_data, x = lstat, y = medv)),
+  tar_target(lm_fit, lm(medv ~ lstat, data = boston_data)),
+  tar_target(lm_pred, predict(lm_fit))
 )
