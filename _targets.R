63259d7c6f108fe477341e5aca8a43d94fc4a47c
Francis Duval <francisduval37@gmail.com>
2022-03-18 11:38:24 -0400

    Target for scatter plot

--- _targets.R
+++ _targets.R
@@ -1,7 +1,10 @@
 library(targets)
 library(tidyverse)
 
+walk(fs::dir_ls("R"), source)
+
 list(
   tar_target(boston_file, "data/boston.csv", format = "file"),
-  tar_target(boston_data, read_csv(boston_file))
+  tar_target(boston_data, read_csv(boston_file)),
+  tar_target(plot_medv_lstat, make_scatter_plot(boston_data, x = lstat, y = medv))
 )
