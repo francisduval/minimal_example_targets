69fd2a575fef41f64765da608a65ce793c34d9ef
Francis Duval <francisduval37@gmail.com>
2022-03-18 11:28:43 -0400

    Target for loading Boston data

--- _targets.R
+++ _targets.R
@@ -1,5 +1,7 @@
 library(targets)
+library(tidyverse)
 
 list(
-  tar_target(boston_file, "data/boston.csv", format = "file")
+  tar_target(boston_file, "data/boston.csv", format = "file"),
+  tar_target(boston_data, read_csv(boston_file))
 )
